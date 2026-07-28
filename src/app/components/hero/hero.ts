83aa23b3aefd28f4b3b521db78c66fff4f2e1165
jimy <jimmyhd_19@hotmail.com>
2026-07-28 00:20:11 -0500

    actualizacion de fecha

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,31 +1,26 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-hero',
-  standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  styleUrl: './hero.scss',
-  template: `
-    <section class="hero">
-      <img
-        src="/images/machu-picchu.svg"
-        alt="Machu Picchu illustration"
-        class="hero__bg-image"
-      />
-      <div class="hero__content">
-        <p class="hero__date">12 DE SEPTIEMBRE DE 2026 | LIMA - PERÚ</p>
-        <h1 class="hero__title">Ng Conf Perú<br />2026</h1>
-        <p class="hero__description">
-          ¡Únete a nosotros para una experiencia única en una conferencia sobre
-          Angular en Perú! Descubre lo último en desarrollo de Angular y conecta
-          con expertos y otros desarrolladores.
-        </p>
-        <div class="hero__actions">
-          <a href="#" class="btn-primary">Comprar entradas</a>
-          <a href="#" class="btn-outline">Postular tu charla</a>
-        </div>
-      </div>
-    </section>
-  `,
+	selector: 'app-hero',
+	standalone: true,
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	styleUrl: './hero.scss',
+	template: `
+		<section class="hero">
+			<img src="/images/machu-picchu.svg" alt="Machu Picchu illustration" class="hero__bg-image" />
+			<div class="hero__content">
+				<p class="hero__date">9 y 10 DE OCTUBRE DE 2026 | LIMA - PERÚ</p>
+				<h1 class="hero__title">Ng Conf Perú<br />2026</h1>
+				<p class="hero__description">
+					¡Únete a nosotros para una experiencia única en una conferencia sobre Angular en Perú! Descubre lo último en
+					desarrollo de Angular y conecta con expertos y otros desarrolladores.
+				</p>
+				<div class="hero__actions">
+					<a href="#" class="btn-primary">Comprar entradas</a>
+					<a href="#" class="btn-outline">Postular tu charla</a>
+				</div>
+			</div>
+		</section>
+	`,
 })
 export class Hero {}
